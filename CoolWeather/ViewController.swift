2302a1fc2efbc7ad7b16618859fc25621c1f35f9
Vincent Moyo <vmoyo2@jhb.dvt.co.za>
2021-08-30 10:19:26 +0200

    Initial Commit

---
+++ CoolWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoolWeather
+//
+//  Created by Vincent Moyo on 2021/08/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
